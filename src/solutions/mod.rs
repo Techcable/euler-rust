9fd2d0237ba7729d48eeb879478fe6ec009bb6fa
Techcable <Techcable@techcable.net>
2019-09-04 17:25:48 -0700

    Revert "Start working on prime_pair_sets"
    Doesn't compile.....
    
    This reverts commit 6bc67825e9ad2b1fdfd6eefd817494139abbfd96.

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -7,7 +7,6 @@ mod powerful_digit_sum;
 mod square_root_convergents;
 mod spiral_primes;
 mod xor_decryption;
-mod prime_pair_sets;
 
 pub trait EulerSolution: Sized + ::std::fmt::Display {
     #[inline]
@@ -74,8 +73,7 @@ pub fn create_problem(name: &str) -> Result<EulerProblem, Error> {
         prime_digit_replacements,
         powerful_digit_sum,
         square_root_convergents,
-        spiral_primes, xor_decryption,
-        prime_pair_sets
+        spiral_primes, xor_decryption
     })
 }
 
